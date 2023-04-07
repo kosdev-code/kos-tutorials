0addc0ac42298196b4bc73ed5cf92bbf0c955e5f
rstoecklein <rstoecklein@coca-cola.com>
2023-04-07 14:17:19 -0500

    Working on very first kOS system app.

--- src/main/java/com/tccc/kos/tutorials/a1_helloworld/MyApp.java
+++ src/main/java/com/tccc/kos/tutorials/a1_helloworld/MyApp.java
@@ -3,8 +3,35 @@
  */
 package com.tccc.kos.tutorials.a1_helloworld;
 
+import com.tccc.kos.commons.core.service.config.ConfigBean;
+import com.tccc.kos.core.service.app.SystemApplication;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class MyApp {
+public class MyApp extends SystemApplication {
+
+    @Override
+    public void load() {
+        log.info("MyApp.load()");
+    }
+
+    @Override
+    public void start() {
+        log.info("MyApp.start()");
+    }
+
+    @Override
+    public void stop() {
+        log.info("MyApp.stop()");
+    }
+
+    @Override
+    public void unload() {
+        log.info("MyApp.unload()");
+    }
+
+    @Override
+    public void setConfig(ConfigBean configBean) {
+        log.info("MyApp.setConfig()");
+    }
 }
