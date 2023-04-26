adf41553f8b25f84ac97ffc0bf9fc7cdd13be6df
rstoecklein <rstoecklein@coca-cola.com>
2023-04-26 14:47:01 -0500

    Removed undesired overridden methods from MyApp.

--- getting-started/src/main/java/com/tccc/kos/tutorials/getting_started/MyApp.java
+++ getting-started/src/main/java/com/tccc/kos/tutorials/getting_started/MyApp.java
@@ -3,7 +3,6 @@
  */
 package com.tccc.kos.tutorials.getting_started;
 
-import com.tccc.kos.commons.core.service.config.ConfigBean;
 import com.tccc.kos.core.service.app.SystemApplication;
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,20 +17,5 @@ public class MyApp extends SystemApplication {
     @Override
     public void start() {
         log.info("MyApp.start()");
-    }
-
-    @Override
-    public void stop() {
-        log.info("MyApp.stop()");
-    }
-
-    @Override
-    public void unload() {
-        log.info("MyApp.unload()");
-    }
-
-    @Override
-    public void setConfig(ConfigBean configBean) {
-        log.info("MyApp.setConfig()");
     }
 }
