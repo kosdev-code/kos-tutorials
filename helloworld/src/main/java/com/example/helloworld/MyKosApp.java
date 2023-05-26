a9baa970017cc89e3547183b129efc02ca7cf0c8
rob-stoecklein <rstoeck@gmail.com>
2023-05-26 08:50:33 -0500

    Fixed descriptor.json and renamed application to MyKosApp.

--- helloworld/src/main/java/com/example/helloworld/MyKosApp.java
+++ helloworld/src/main/java/com/example/helloworld/MyKosApp.java
@@ -3,19 +3,20 @@
  */
 package com.example.helloworld;
 
+import com.tccc.kos.core.service.app.AppConfig;
 import com.tccc.kos.core.service.app.SystemApplication;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class MyApp extends SystemApplication {
+public class MyKosApp extends SystemApplication<AppConfig> {
 
     @Override
     public void load() {
-        log.info("MyApp.load()");
+        log.info("MyKosApp.load()");
     }
 
     @Override
     public void start() {
-        log.info("MyApp.start()");
+        log.info("MyKosApp.start()");
     }
 }
