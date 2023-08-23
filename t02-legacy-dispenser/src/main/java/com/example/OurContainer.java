e40149d1acddea880a85ae686f47738c0362b041
rob-stoecklein <rstoeck@gmail.com>
2023-08-23 09:43:23 -0500

    Fixed a comment.

--- t02-legacy-dispenser/src/main/java/com/example/OurContainer.java
+++ t02-legacy-dispenser/src/main/java/com/example/OurContainer.java
@@ -5,8 +5,7 @@ import com.tccc.kos.ext.dispense.ContainerSlice;
 
 /**
  * Simple container that contains a single slice.
- * <p>
- * Both the {@code Container} and its {@code ContainerSlice} use {@code ingredientId} as their IDs, respectively.
+ * Both the Container and its ContainerSlice use ingredientId as their IDs.
  */
 public class OurContainer extends Container {
 
