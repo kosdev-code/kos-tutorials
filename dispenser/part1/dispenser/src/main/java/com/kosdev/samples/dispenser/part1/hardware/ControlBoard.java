089cda64caf8b73ca9aa7e65f721f295fe56cf0f
davidwburns <davidwburns@yahoo.com>
2024-10-18 16:18:41 -0500

    check in new tutorial code for access from the tutorials

---
+++ dispenser/part1/dispenser/src/main/java/com/kosdev/samples/dispenser/part1/hardware/ControlBoard.java
@@ -0,0 +1,78 @@
+package com.kosdev.samples.dispenser.part1.hardware;
+
+import com.tccc.kos.commons.util.KosUtil;
+import com.tccc.kos.commons.util.concurrent.future.FutureEvent;
+import com.tccc.kos.commons.util.concurrent.future.FutureWork;
+import com.tccc.kos.core.service.assembly.Assembly;
+import com.tccc.kos.ext.dispense.Pump;
+import com.tccc.kos.ext.dispense.PumpBoard;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Models a simple control board that contains one water valve and
+ * four syrup valves. This is modeled after typical hardware where
+ * each valve is assigned a position and turning valves on and off
+ * involves sending the list of valve positions to the actual hardware
+ * as part of a pour command.
+ *
+ * @since 1.0
+ * @version 2024-09-23
+ */
+@Slf4j
+@Getter
+@Setter
+public class ControlBoard extends PumpBoard {
+    private final Valve waterValve;
+    private final List<Pump<?>> syrupValves;
+
+    public ControlBoard(Assembly assembly, String name) {
+        super(assembly, name);
+
+        // Board contains one water valve and four syrup valves
+        waterValve = new Valve(this, "water", 0);
+        syrupValves = new ArrayList<>();
+        for (int i = 1; i <= 4; i++) {
+            syrupValves.add(new Valve(this, "syrup-" + i, i));
+        }
+    }
+
+    /**
+     * Valve.tpour() calls this, allowing a central location to manage
+     * the list of valve positions that should be on or off. This sample
+     * code simply logs the pump state using a timer.
+     */
+    public FutureWork tpour(Valve pump, int duration, double rate) {
+        // Create a future to turn the pump on for the requested duration
+        FutureWork future = new FutureWork("tpour" + "-" + pump.getName(), f -> {
+            log.info("start: {}", pump.getName());
+            pump.recordRate(pump.getConfig().getNominalRate());
+            KosUtil.scheduleCallback(() -> f.success(), duration);
+            // TODO: turn on the valve
+        });
+
+        // Add a completion handler to turn off the valve, which is
+        // called when the timer fires or if the pour is cancelled
+        future.append("stop", FutureEvent.COMPLETE, f -> {
+            log.info("stop: {}", pump.getName());
+            pump.recordRate(0);
+            // TODO: turn off the valve
+        });
+
+        return future;
+    }
+
+    @Override
+    public String getType() {
+        return "controlBoard";
+    }
+
+    @Override
+    public String getInstanceId() {
+        return null;
+    }
+}
---
+++ dispenser/part1/dispenser/src/main/java/com/kosdev/samples/dispenser/part1/pour/DispenserPourEngine.java
@@ -0,0 +1,102 @@
+package com.kosdev.samples.dispenser.part1.pour;
+
+import com.kosdev.samples.dispenser.part1.DispenserApp;
+import com.kosdev.samples.dispenser.part1.brandset.Beverage;
+import com.kosdev.samples.dispenser.part1.brandset.Brandset;
+import com.tccc.kos.commons.core.context.annotations.Autowired;
+import com.tccc.kos.commons.util.concurrent.future.FailedFuture;
+import com.tccc.kos.commons.util.concurrent.future.FutureWork;
+import com.tccc.kos.commons.util.concurrent.future.ParallelFuture;
+import com.tccc.kos.ext.dispense.Pump;
+import com.tccc.kos.ext.dispense.pipeline.beverage.BeveragePourEngine;
+import com.tccc.kos.ext.dispense.pipeline.beverage.BeveragePourEngineConfig;
+import com.tccc.kos.ext.dispense.pipeline.beverage.Pourable;
+import com.tccc.kos.ext.dispense.pipeline.beverage.RecipeExtractor;
+import com.tccc.kos.ext.dispense.pipeline.beverage.graph.BevGraphBuilder;
+import com.tccc.kos.ext.dispense.pipeline.beverage.graph.BeverageNode;
+
+/**
+ * This is used abstract a way to pour a beverage
+ *
+ * @since 1.0
+ * @version 2024-09-09
+ */
+public class DispenserPourEngine extends BeveragePourEngine<BeveragePourEngineConfig> {
+
+    @Autowired
+    private DispenserApp app; // Access to the brandset
+
+    @Override
+    public void start() {
+        // Nothing to do when the engine starts
+    }
+
+    @Override
+    public void stop() {
+        // Nothing to do when the engine stops
+    }
+
+    /**
+     * Builds graph of beverage ingredients that can be used to determine if a beverage is available or not
+     */
+    @Override
+    public void rebuildGraph(BevGraphBuilder builder) {
+        // Add ingredient nodes for all the pumps
+        builder.addIngredientNodes();
+
+        // Get the brandset from the app
+        Brandset brandset = app.getBrandset();
+
+        // Process beverages from the brandset
+        for (Beverage bev : brandset.getBeverages()) {
+            // add the beverage to the graph
+            builder.addBeverage(new BeverageNode(bev.getId()).setNote(bev.getName()));
+
+            // beverage depends on associated ingredients
+            for (String ingredientId : bev.getIngredientIds()) {
+                builder.addOptionalDependency(bev.getId(), ingredientId);
+            }
+        }
+    }
+
+    @Override
+    public Pourable getPourable(String bevId) throws Exception {
+        return new BevPourable(bevId);
+    }
+
+    @Override
+    public boolean isPourable(Pourable pourable) throws Exception {
+        // Can be poured if the beverage is available
+        return isAvailable(((BevPourable) pourable).getBevId());
+    }
+
+    @Override
+    protected FutureWork buildFuture(Pourable pourable, Object lock) {
+
+        String bevId = ((BevPourable) pourable).getBevId();
+
+        // Create recipe extractor to extract the pumps to use
+        RecipeExtractor extractor = new RecipeExtractor(this).addIngredients(bevId);
+
+        // If the extractor didn't find a way to pour, return a failed future
+        if (!extractor.isValid()) {
+            return new FailedFuture("pour", "errNotPourable");
+        }
+
+        // Sum the flow rates of all the pumps in the recipe to get the overall flow rate
+        double totalRate = extractor.getPumps().stream()
+                .mapToDouble(Pump::getNominalRate)
+                .sum();
+
+        // Compute duration of the pour based on effective volume and total flow rate
+        int durationMs = (totalRate > 0) ? (int) (pourable.getEffectiveVolume() * 1000 / totalRate) : 0;
+
+        // Tpour all the valves in the recipe for the specified duration
+        ParallelFuture future = new ParallelFuture("pour");
+        for (Pump<?> pump : extractor.getPumps()) {
+            future.add(pump.tpour(durationMs, 0));
+        }
+
+        return future;
+    }
+}
