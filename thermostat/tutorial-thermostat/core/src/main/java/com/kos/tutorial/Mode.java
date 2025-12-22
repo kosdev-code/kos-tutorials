a5a471559d101ba090254b4bf974d48971fcb021
Sneh Gupta <sneh@kondra.com>
2025-12-22 13:03:55 -0500

    Part C tutorial: done

---
+++ thermostat/tutorial-thermostat/core/src/main/java/com/kos/tutorial/Mode.java
@@ -0,0 +1,29 @@
+/**
+ * (C) Copyright 2025 Kondra. All rights reserved.
+ */
+package com.kos.tutorial;
+
+import com.tccc.kos.sdk.annotations.Sdk;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * The mode of the thermostat: off, heating, cooling
+ *
+ * @author Sneh Gupta (sneh@kondra.com)
+ * @version 2025-12
+ */
+@Sdk
+public enum Mode {
+    OFF {
+        @Override public String getColor() { return "#AFB2B4"; }
+    },
+    HEATING {
+        @Override public String getColor() { return "#FFA69E"; }
+    },
+    COOLING {
+        @Override public String getColor() { return "#83C9F4"; }
+    };
+
+    public abstract String getColor();
+}
---
+++ thermostat/tutorial-thermostat/sdk/src/main/java/com/kos/tutorial/Mode.java
@@ -0,0 +1,21 @@
+/**
+ * (C) Copyright 2025 Kondra. All rights reserved.
+ */
+
+package com.kos.tutorial;
+
+/**
+ * The mode of the thermostat: off, heating, cooling
+ *
+ * @author Sneh Gupta (sneh@kondra.com)
+ * @version 2025-12
+ */
+public enum Mode {
+    OFF,
+    HEATING,
+    COOLING;
+
+    public String getColor() {
+        return null;
+    }
+}
