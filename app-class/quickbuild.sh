563eed81ba03f0fce41f640e22ce44ae53b2fdde
Sneh Gupta <sneh@kondra.com>
2026-04-03 15:55:10 -0400

    Fix

--- app-class/quickbuild.sh
+++ app-class/quickbuild.sh
@@ -2,8 +2,8 @@
 set -e -o pipefail -u
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-echo "Building thermostat..."
-"$SCRIPT_DIR/system-app/quickbuild.sh"
-
 echo "Building thermostat simulator..."
 "$SCRIPT_DIR/device-management/quickbuild.sh"
+
+echo "Building thermostat..."
+"$SCRIPT_DIR/system-app/quickbuild.sh"
