33f055d18ed120304a692c4248c09e5310522f5a
jcPringle <112016506+jcPringle@users.noreply.github.com>
2026-09-02 12:58:32 -0400

    testing the importer

--- thermostat/java/class/thermostat/core/src/main/java/com/kos/tutorial/Main.java
+++ thermostat/java/class/thermostat/core/src/main/java/com/kos/tutorial/Main.java
@@ -5,7 +5,7 @@ package com.kos.tutorial;
 
 public class Main {
     /**
-     * This is just for the debugger;
+     * This is just for using the debugger;
      */
     public static void main(String[] args) throws Exception {
         System.out.println("Hello World!");
